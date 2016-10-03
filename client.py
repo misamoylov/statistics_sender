9190d6bfe73b0a27e01ab2141e79c0287e1a560c
misamoylov <mikhailsamoiloff@gmail.com>
2016-10-03 21:48:34 +0300

    1

--- client.py
+++ client.py
@@ -1,12 +1,22 @@
 # coding=utf-8
 import wmi
 comp = wmi.WMI()
-http://timgolden.me.uk/python/wmi/cookbook.html - документация на wmi
+# http://timgolden.me.uk/python/wmi/cookbook.html - документация на wmi
 for i in comp.Win32_ComputerSystem():
-   print i.TotalPhysicalMemory, "bytes of physical memory"
-
+   print(i.TotalPhysicalMemory, "bytes of physical memory")
 for os in comp.Win32_OperatingSystem():
-   print os.FreePhysicalMemory, "bytes of available memory"
+   print(os.FreePhysicalMemory, "bytes of available memory")
+c = wmi.WMI(privileges=["Security"])
+
+watcher = c.watch_for(
+  notification_type="Creation",
+  wmi_class="Win32_NTLogEvent",
+  Type="error"
+)
+while 1:
+  error = watcher()
+  print "Error in %s log: %s" % (error.Logfile, error.Message)
+  # send mail to sysadmin etc.
 uptime = 'systeminfo | find "System Boot Time:"'
 memory = ''
 cpu_info = ''
