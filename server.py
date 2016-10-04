3075c921a2eb027aabf72879e704594572fb1e96
Mikhail Samoylov <m.samoylov@postgrespro.ru>
2016-10-04 18:27:23 +0300

    4

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import smtplib
 import sqlite3
+import os
 import xml.etree.ElementTree as ET
+
+from settings import CONFIG_PATH
 
 class Server(object):
     def __init__(self, dbname):
@@ -13,6 +16,16 @@ class Server(object):
     def xml2db(self):
         return ET.parse('/home/msamoylov/statistics_sender/configs/user1.xml')
 
+    def get_hosts(self, configs):
+        hosts = []
+        for config in configs:
+            tree = ET.parse(CONFIG_PATH+config)
+            root = tree.getroot()
+            return root.attrib['ip']
+
+
+    def get_configs(self):
+        return os.listdir(CONFIG_PATH)
 
     def resp2db(self, response):
         pass
