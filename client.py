0ac9bd12cfd11117d025d3a958a8323a6089c231
Mikhail Samoylov <m.samoylov@postgrespro.ru>
2016-10-04 18:14:26 +0300

    4

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import wmi
 import psutil
 from uptime import uptime
 import os
-comp = wmi.WMI('192.168.122.113', user='Administrator', password='TestRoot1')
+comp = wmi.WMI()
 # http://timgolden.me.uk/python/wmi/cookbook.html - документация на wmi
 for i in comp.Win32_ComputerSystem():
    print(i.TotalPhysicalMemory, "bytes of physical memory")
@@ -22,20 +22,25 @@ for os in comp.Win32_OperatingSystem():
 #   error = watcher()
 #   print "Error in %s log: %s" % (error.Logfile, error.Message)
 #   # send mail to sysadmin etc.
-# uptime = 'systeminfo | find "System Boot Time:"'
-# memory = ''
-# cpu_info = ''
 
-def get_uptime():
-    return uptime()
+def get_host_info(Windows=False):
 
-def get_mem_linux():
-    # sswap(total=2097147904L, used=886620160L, free=1210527744L, percent=42.3, sin=1050411008, sout=1906720768)
-    return str(psutil.virtual_memory().percent)
+    def get_uptime():
+        return uptime()
 
-def get_cpu_linux():
-    """
+    def get_mem():
+        return str(psutil.virtual_memory().percent)
 
-    :return: float
-    """
-    return str(psutil.cpu_percent())
+    def get_cpu():
+        """
+
+        :return: float
+        """
+        return str(psutil.cpu_percent())
+
+    host_info = {'host': "",
+                 'uptime': get_uptime(),
+                 'memory': get_mem(),
+                 'cpu': get_cpu(),
+                 }
+    return host_info
--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 import sqlite3
-# id - Integer, host id
-# IP_addr - 'string' ip address of host
+# IP_addr - 'string' ip address of host, primary key
 # OS - string operating system
 # User - username for auth
 # Passowrd - password for auth
@@ -11,15 +10,30 @@ import sqlite3
 # Limit Average Memory - Integer
 
 
-conn = sqlite3.connect('example.db')
+conn = sqlite3.connect('example3.db')
 c = conn.cursor()
 # Create table
 c.execute('''CREATE TABLE hosts
-             (id integer primary key,
-              ip_address text, OS text, user VARCHAR(100),
+             (ip_address text primary key, OS text, user VARCHAR(100),
                password text, mail text,
                av_cpu text, lim_cpu text, av_mem text,
                lim_mem text, last_sec_log_event text, uptime text)''')
+# Save (commit) the changes
+conn.commit()
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
+
+
+conn = sqlite3.connect('example2.db')
+c = conn.cursor()
+# Create table
+c.execute('''CREATE TABLE hosts
+             (id integer not null,
+              ip_address text not null, OS text, user VARCHAR(100),
+               password text, mail text,
+               av_cpu text, lim_cpu text, av_mem text,
+               lim_mem text, last_sec_log_event text, uptime text, PRIMARY KEY (id, ip_address)''')
 # Save (commit) the changes
 conn.commit()
 # We can also close the connection if we are done with it.
