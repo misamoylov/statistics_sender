8fc363d38a51241198386562e23b4ac7b92d8d4f
Mikhail Samoylov <m.samoylov@postgrespro.ru>
2016-10-05 20:50:19 +0300

    4

--- client.py
+++ client.py
@@ -3,7 +3,8 @@ import psutil
 from uptime import uptime
 import os
 from Crypto.Cipher import AES
-
+#http://www.blog.pythonlibrary.org/2010/07/27/pywin32-getting-windows-event-logs/
+#http://code.activestate.com/recipes/577499-windows-event-log-viewer/
 class Client(object):
     def __init__(self):
         self.cryptor = AES.new('key', AES.MODE_CBC, 'IV456')
