c5a78cfcd64c8da920329ba80f18acd171dd1f33
misamoylov <mikhailsamoiloff@gmail.com>
2016-10-06 23:04:48 +0300

    Merge branch 'master' of https://github.com/misamoylov/statistics_sender
    # Conflicts:
    #	client.py
    #	runner.py
    #	server.py

--- client.py
+++ client.py
@@ -1,20 +1,36 @@
 # coding=utf-8
 import psutil
 import platform
+import win32evtlog
 
 from cryptography.fernet import Fernet
 from uptime import uptime
 key = 'cpB2--8hBT7qbXjZW7QQwYolI6g39p96bslIVAMZ7kA='
 
 class Client(object):
+    # just for example
 
     def __init__(self):
         self.fernet = Fernet(key)
+        self.security_events_ids = ['4771', '675', '676', '4768']
+        self.win_server = 'localhost'
+        self.logtype = 'System'
 
     def check_windows_security_events(self):
         """ Check windows security events
         :return: str 'yes' or 'no
         """
+        hand = win32evtlog.OpenEventLog(self.win_server, self.logtype)
+        flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
+        events = win32evtlog.ReadEventLog(hand, flags, 0)
+        answer = 'no'
+        for event in events:
+            if event.EventID in self.security_events_ids:
+                answer = 'yes'
+                break
+            else:
+                return answer
+        return answer
 
     def get_host_info(self):
 
@@ -27,12 +43,12 @@ class Client(object):
         def get_cpu():
             return str(psutil.cpu_percent())
 
-        if platform.system is 'Windows':
+        if platform.system() is 'Windows':
             host_info = {"uptime": self.fernet.encrypt(get_uptime()),
                          "memory": self.fernet.encrypt(get_mem()),
                          "cpu": self.fernet.encrypt(get_cpu()),
                          "os": self.fernet.encrypt(platform.system()),
-                         "log_message": ""
+                         "log_message": self.fernet.encrypt(self.check_windows_security_events())
                          }
         else:
             host_info = {"uptime": self.fernet.encrypt(get_uptime()),
--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ import os
 CONFIG_PATH = os.environ.get('CONFIG_PATH')
 CLIENT_SCRIPT = os.environ.get('CLIENT_SCRIPT')
 MAIL_LOGIN = 'statsts497@gmail.com'
-MAIL_PASSWORD = ''
+MAIL_PASSWORD = 'statsts49712345'
 FERNET_KEY = 'cpB2--8hBT7qbXjZW7QQwYolI6g39p96bslIVAMZ7kA='
